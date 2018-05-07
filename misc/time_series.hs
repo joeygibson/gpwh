44cf30d38226cad36e0e62cc01298d71501b8d72
Joey Gibson <joey@joeygibson.com>
2018-05-06 21:42:51 -0700

    more time series

--- misc/time_series.hs
+++ misc/time_series.hs
@@ -64,6 +64,46 @@ combineTS (TS t1 v1) (TS t2 v2) = TS completeTimes combinedValues
           updatedMap = foldl insertMaybePair tvMap (zip t2 v2)
           combinedValues = map (\v -> Map.lookup v updatedMap) completeTimes
 
+mean :: (Real a) => [a] -> Double
+mean xs = total/count
+    where total = (realToFrac . sum) xs
+          count = (realToFrac . length) xs
+
+meanTS :: (Real a) => TS a -> Maybe Double
+meanTS (TS _ []) = Nothing
+meanTS (TS times values) = if all (== Nothing) values
+                           then Nothing
+                           else Just avg
+    where justVals = filter isJust values
+          cleanVals = map fromJust justVals
+          avg = mean cleanVals
+
+type CompareFunc a = a -> a -> a
+type TSCompareFunc a = (Int, Maybe a) -> (Int, Maybe a) -> (Int, Maybe a)
+
+makeTSCompare :: Eq a => CompareFunc a -> TSCompareFunc a
+makeTSCompare func = newFunc
+    where newFunc (i1, Nothing) (i2, Nothing) = (i1, Nothing)
+          newFunc (_, Nothing) (i, val) = (i, val)
+          newFunc (i, val) (_, Nothing) = (i, val)
+          newFunc (i1, Just val1) (i2, Just val2) = if func val1 val2 == val1
+                                                    then (i1, Just val1)
+                                                    else (i2, Just val2)
+
+compareTS :: Eq a => (a -> a -> a) -> TS a -> Maybe (Int, Maybe a)
+compareTS func (TS [] []) = Nothing
+compareTS func (TS times values) = if all (== Nothing) values
+                                   then Nothing
+                                   else Just best
+    where pairs = zip times values
+          best = foldl (makeTSCompare func) (0, Nothing) pairs
+
+minTS :: Ord a => TS a -> Maybe (Int, Maybe a)
+minTS = compareTS min
+
+maxTS :: Ord a => TS a -> Maybe (Int, Maybe a)
+maxTS = compareTS max
+
 -- example data
 
 ts1 :: TS Double
